018399fd7e8252ea2c0c31e5540b421b2fa9ffc6
Puneet <pjohal@stuy.edu>
2018-09-28 15:35:40 -0400

    i tried

--- mystr.c
+++ mystr.c
@@ -9,12 +9,24 @@ int mystrlen (char * s){
   return answer; 
 }
 
+char * mystrcpy( char *dest, char *source ){
+  //char * destCopy = *dest;
+  while (* source ){
+    *dest = *source;
+    dest++;
+    source++;
+  }
+  return *dest;
+}
+    
+
 int main(){
   char s[]="";
   char s0[]="hello";
   char s1[]="goodbye";
   printf("length of empty string: %d\n", mystrlen(s)); 
   printf("length of hello: %d\n", mystrlen(s0));
-  printf("length of goodbye: %d\n", mystrlen(s1)); 
+  printf("length of goodbye: %d\n", mystrlen(s1));
+  printf("copying s0 into s1: %s\n", mystrcpy(s1,s0));
   return 0;
 }
