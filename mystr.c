a63ea34592227cdffc836465ff188603085d8a28
Adil Gondal <agondal@stuy.edu>
2018-09-28 15:17:12 -0400

    test cases

--- mystr.c
+++ mystr.c
@@ -10,5 +10,11 @@ int mystrlen (char * s){
 }
 
 int main(){
+  char s[]="";
+  char s0[]="hello";
+  char s1[]="goodbye";
+  printf("length of empty string: %d\n", mystrlen(s)); 
+  printf("length of hello: %d\n", mystrlen(s0));
+  printf("length of goodbye: %d\n", mystrlen(s1)); 
   return 0;
 }
