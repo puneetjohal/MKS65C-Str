48448a706c5ddbe6854668bcd71dcaaa1830f0c0
Adil Gondal <agondal@stuy.edu>
2018-09-30 18:01:10 -0400

    mystrcpy

--- mystr.c
+++ mystr.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 
+//gives length of string
 int mystrlen (char * s){
   int answer=0;
   while(*s++){
@@ -9,14 +10,16 @@ int mystrlen (char * s){
   return answer; 
 }
 
+//copies source string to dest string
 char * mystrcpy( char *dest, char *source ){
   //char * destCopy = *dest;
+  int i=0;
   while (* source ){
-    *dest = *source;
-    dest++;
-    source++;
-  }
-  return *dest;
+    *dest++ = *source++;
+    i++;
+    }
+  *(dest)='\0';
+  return (dest-i);
 }
     
 
@@ -24,9 +27,12 @@ int main(){
   char s[]="";
   char s0[]="hello";
   char s1[]="goodbye";
-  printf("length of empty string: %d\n", mystrlen(s)); 
-  printf("length of hello: %d\n", mystrlen(s0));
-  printf("length of goodbye: %d\n", mystrlen(s1));
-  printf("copying s0 into s1: %s\n", mystrcpy(s1,s0));
+  //printf("length of empty string: %d\n", mystrlen(s)); 
+  //printf("length of hello: %d\n", mystrlen(s0));
+  //printf("length of goodbye: %d\n", mystrlen(s1));
+  //printf("copying s0 into s1: %s\n", mystrcpy(s1,s0));
+  //printf("copying s0 into s1: %s\n", strcpy(s1,s0));
+  //printf("copying s into s0: %s\n", mystrcpy(s0,s));
+  //printf("copying s into s0: %s\n", strcpy(s0,s));
   return 0;
 }
